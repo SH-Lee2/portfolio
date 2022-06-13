a3931a5f877ce588fc482fac5e02932b03e32365
SH-Lee2 <tlgud585@gmail.com>
2022-06-13 21:21:52 +0900

    .

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,12 +2,26 @@ import React from "react";
 import { Container } from "../../styles/Container.styled";
 import { Section } from "../../styles/Section.styled";
 import Transitions from "../../Transitions";
+import lightHome from "../../../assets/home-light.png";
+import darkHome from "../../../assets/home-dark.png";
 
+import { HomeImage, Title } from "../../styles/Home.styled";
+import { useTheme } from "styled-components";
+import { Link } from "react-router-dom";
 const Home = () => {
+    const { theme } = useTheme();
+    const image = theme === "dark" ? darkHome : lightHome;
+    const greetText = "안녕하세요\n프론트엔드 개발자\n이시형 입니다.";
     return (
         <Transitions>
             <Section>
-                <Container></Container>
+                <Container>
+                    <div>
+                        <Title>{greetText}</Title>
+                        <HomeImage src={image} alt="홈 이미지" />
+                    </div>
+                    <Link to="contact">Contact ME</Link>
+                </Container>
             </Section>
         </Transitions>
     );
---
+++ src/components/styles/Home.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    white-space: pre-wrap;
+    font-weight: 700;
+    letter-spacing: 2px;
+    line-height: 4rem;
+    font-size: 3rem;
+    color: ${({ theme }) => theme.colors.mainColor1};
+`;
+
+export const HomeImage = styled.img``;
